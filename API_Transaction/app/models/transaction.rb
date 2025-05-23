77945cd6d5d76edcfb0c757c9c3ded9edfb64342
Alejandra Garcia <yazmin.alejandragh@gmail.com>
2025-05-23 09:11:45 -0600

    Se agregaron validaciones a transactions y se hizo test al modelo

--- API_Transaction/app/models/transaction.rb
+++ API_Transaction/app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validates :sender, :receiver, :coin_to_send, :coin_to_receive, :amount_to_send, :amount_to_receive, presence: true
+  validates :amount_to_send, :amount_to_receive, numericality: true
 end
--- API_Transaction/spec/models/transaction_spec.rb
+++ API_Transaction/spec/models/transaction_spec.rb
@@ -4,4 +4,64 @@ RSpec.describe Transaction, type: :model do
   it 'has_factory' do
     expect(create(:transaction)).to be_persisted
   end
+
+  it "is not valid without a sender" do
+    transaction = Transaction.new(
+      sender: nil,
+      receiver: create(:user),
+      coin_to_send: 'usd',
+      coin_to_receive: 'btc',
+      amount_to_send: 100)
+    expect(transaction).to_not be_valid
+  end
+
+  it "is not valid without a receiver" do
+    transaction = Transaction.new(
+      receiver: nil,
+      sender: create(:user),
+      coin_to_send: 'usd',
+      coin_to_receive: 'btc',
+      amount_to_send: 100)
+    expect(transaction).to_not be_valid
+  end
+
+  it "is not valid without a coin_to_send" do
+    transaction = Transaction.new(
+      coin_to_send: nil,
+      sender: create(:user),
+      receiver: create(:user),
+      coin_to_receive: 'btc',
+      amount_to_send: 100)
+    expect(transaction).to_not be_valid
+  end
+
+  it "is not valid without a coin_to_receive" do
+    transaction = Transaction.new(
+      coin_to_receive: nil,
+      sender: create(:user),
+      receiver: create(:user),
+      coin_to_send: 'btc',
+      amount_to_send: 100)
+    expect(transaction).to_not be_valid
+  end
+
+  it "is not valid without a amount_to_send" do
+    transaction = Transaction.new(
+      amount_to_send: nil,
+      sender: create(:user),
+      receiver: create(:user),
+      coin_to_send: 'btc',
+      coin_to_receive: 'usd')
+    expect(transaction).to_not be_valid
+  end
+
+  it "is not valid if amount_to_send is not a number" do
+    transaction = Transaction.new(
+      amount_to_send: "10",
+      sender: create(:user),
+      receiver: create(:user),
+      coin_to_send: 'btc',
+      coin_to_receive: 'usd')
+    expect(transaction).to_not be_valid
+  end
 end
